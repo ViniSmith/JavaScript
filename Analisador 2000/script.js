005f62551b53830da8c333350a1ca47a24310c9b
Alfe <vinicius123.malec@hotmail.com>
2021-03-26 16:24:20 -0300

    Arrumando os erros no script.

--- Analisador 2000/script.js
+++ Analisador 2000/script.js
@@ -1,3 +1,14 @@
 function verificar() {
-    window.alert('funcionou')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')[0]
+        var idade = ano - fano.value
+        
+    }
+    
 }
