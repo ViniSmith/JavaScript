136233c1a37f0b5c90585e7b1dc3d3042138c920
Alfe <vinicius123.malec@hotmail.com>
2021-03-29 18:27:32 -0300

    criando o css para o contador

---
+++ Contador/script.js
@@ -0,0 +1,34 @@
+function contar() {
+    var fini = document.getElementById('txtini')
+    var ini = Number(fini.value)
+    var ffim = document.getElementById('txtfim')
+    var fim = Number(ffim.value)
+    var fpass = document.getElementById('txtpas')
+    var pass = Number(fpass.value)
+    var res = document.getElementById('res')
+
+    if (fini.value == 0 || ffim.value == 0 || fpass.value == 0) {
+        res.innerHTML = 'Impossível contar'
+        window.alert('[ERRO]')
+
+    }else {
+        res.innerText = 'Contanto:\u{1F633}'
+
+            if (pass <= 0) {
+                window.alert('Sequência inválida! Considerando 1 ao invés')
+                pass = 1
+            }
+
+            if (ini<fim) { //CONTAGEM CRESCENTE
+                for(let c = ini ; c <= fim ; c += pass) {
+                    res.innerHTML += `${c} \u{1F921} `
+                }
+                res.innerHTML += `${c} \u{1F60E}`
+            } else { // CONTAGEM DECRESCENTE
+                for(let c = ini; c>=fim; c-= pass) {
+                    res.innerHTML += `${c} \u{1F921}`
+                }
+                res.innerHTML += `\u{1F60E}`                    
+            }
+        }
+}
