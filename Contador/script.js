71e3e99099d41873db8a07ffc6ec63ad57f2c152
Alfe <vinicius123.malec@hotmail.com>
2021-03-30 15:11:54 -0300

    Pequeno erro no script do contador consertado

--- Contador/script.js
+++ Contador/script.js
@@ -21,9 +21,10 @@ function contar() {
 
             if (ini<fim) { //CONTAGEM CRESCENTE
                 for(let c = ini ; c <= fim ; c += pass) {
-                    res.innerHTML += `${c} \u{1F921} `
+                    res.innerHTML += `${c} \u{1F921}`
                 }
-                res.innerHTML += `${c} \u{1F60E}`
+                res.innerHTML += `\u{1F60E}`
+                
             } else { // CONTAGEM DECRESCENTE
                 for(let c = ini; c>=fim; c-= pass) {
                     res.innerHTML += `${c} \u{1F921}`
