0e47eb7e8c455981a07697876c35951cf3368c71
Alfe <vinicius123.malec@hotmail.com>
2021-04-30 16:34:56 -0300

    arrumando scripts

--- exercício/script.js
+++ exercício/script.js
@@ -1,1 +1,33 @@
-alert('Olá!')
+var num = document.getElementById('txtnum')
+var lista = document.getElementById('lista')
+var res = document.getElementById('res')
+var valores = []
+
+function isNumber(n) {
+    if(Number(n) >= 1 && Number(n) <=100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumber(num.value) && !inList(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+    } else {
+        window.alert ('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
