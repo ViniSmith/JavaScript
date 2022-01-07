069f90edcc8c04376c4289e05eaf2f2234107b42
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-01-07 10:40:25 -0300

    Realizando exercícios

--- DOM-para-iniciantes/o-que-é-DOM/script.js
+++ DOM-para-iniciantes/o-que-é-DOM/script.js
@@ -9,6 +9,22 @@ if(href ==='http://127.0.0.1:5500/index.html') {
 const h1Selecionado = document.querySelector('h1');
 const h1Classes = h1Selecionado.classList;
 
-h1Selecionado.addEventListener('click', function() {
+function callbackh1() {
     console.log('Clicou em ', h1Selecionado.innerText);
-})
+}
+h1Selecionado.addEventListener('click', callbackh1);
+
+//Exercícios
+// Retorne o url da página atual utilizando o objeto window
+const urlPagina = window.location.href;
+console.log(urlPagina);
+
+//Seleciona o primeiro elemento da página que possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo')
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+//Retorne a largura da janela
+const largura = window.innerWidth;
+console.log(largura);
