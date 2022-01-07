0ffc2b6fb4d53f61b40bc1cca3705a8ab3e51598
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-01-07 10:17:23 -0300

    Utilizando mais elementos DOM

--- DOM-para-iniciantes/o-que-é-DOM/script.js
+++ DOM-para-iniciantes/o-que-é-DOM/script.js
@@ -5,3 +5,10 @@ console.log(href);
 if(href ==='http://127.0.0.1:5500/index.html') {
     console.log("É igual")
 }
+
+const h1Selecionado = document.querySelector('h1');
+const h1Classes = h1Selecionado.classList;
+
+h1Selecionado.addEventListener('click', function() {
+    console.log('Clicou em ', h1Selecionado.innerText);
+})
