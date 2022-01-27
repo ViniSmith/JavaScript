46fe9d36044ee7684fb3c5f48ffb18e119ca6f95
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-01-27 11:13:29 -0300

    Exercício sobre Manipulação de Elementos feito.

--- animais-fantasticos-projeto-curso/scriptExerc6.js
+++ animais-fantasticos-projeto-curso/scriptExerc6.js
@@ -1,7 +1,19 @@
 // Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu');
+const copy = document.querySelector('.copy');
+const cloneMenu = menu.cloneNode(true);
+copy.appendChild(cloneMenu);
 
 // Selecione o primeiro DT da dl de Faq
+const primeiroDt = document.querySelector('.faq dt');
+console.log(primeiroDt);
 
 // Selecione o DD referente ao primeiro DT
+const proximoDD = primeiroDt.nextElementSibling;
+console.log(proximoDD);
 
 // Substitua o conteúdo html de .faq pelo de .animais
+const faq = document.querySelector('.faq');
+const animais = document.querySelector('.animais');
+
+faq.innerHTML = animais.innerHTML;
