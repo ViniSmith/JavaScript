b3b17aa66f6181200d764e6d387422d4804ed22c
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-01-31 11:09:03 -0300

    Finalizando a According List com alguns efeitos de animação

--- animais-fantasticos-projeto-curso/script.js
+++ animais-fantasticos-projeto-curso/script.js
@@ -21,13 +21,24 @@ const tabContent = document.querySelectorAll('.js-tabcontent section');
 }
 initTabNav();
 
-const accordionList = document.querySelectorAll('.js-accordion dt');
 
-function activeAccordion() {
-    this.classList.toggle('ativo'); 
-    this.nextElementSibling.classList.toggle('ativo');
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+    if(accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+
+    function activeAccordion() {
+        this.classList.toggle(activeClass); 
+        this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion);
+    });
 }
-
-accordionList.forEach((item) => {
-    item.addEventListener('click', activeAccordion);
-});
+}
+initAccordion();
