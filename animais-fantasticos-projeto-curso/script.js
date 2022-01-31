d65a61eb1f0b54dc05d0528d171b2c177390dab2
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-01-31 11:16:16 -0300

    Criando um scroll suave ao clicar nos links internos do menu.

--- animais-fantasticos-projeto-curso/script.js
+++ animais-fantasticos-projeto-curso/script.js
@@ -42,3 +42,15 @@ function initAccordion() {
 }
 }
 initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    console.log(href);
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+});
