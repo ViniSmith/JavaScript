091ce3851abf3b4beaa3f01c4954591c7c4e1534
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-01-31 10:49:46 -0300

    Adicionando Accordion List ao faq do site

--- animais-fantasticos-projeto-curso/script.js
+++ animais-fantasticos-projeto-curso/script.js
@@ -19,3 +19,15 @@ const tabContent = document.querySelectorAll('.js-tabcontent section');
         });
     }
 }
+initTabNav();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion() {
+    this.classList.toggle('ativo'); 
+    this.nextElementSibling.classList.toggle('ativo');
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+});
