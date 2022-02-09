05486d993ebf6ef19a87812c4d3e130f61cce38d
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-02-09 12:05:22 -0300

    Exercícios sobre Prototype feitos.

--- animais-fantasticos-projeto-curso/scriptExerc8.js
+++ animais-fantasticos-projeto-curso/scriptExerc8.js
@@ -15,18 +15,28 @@ Pessoas.prototype.nomeCompleto = function() {
 // Liste os métodos acessados por
 // dados criados em NodeList,
 // HTMLCollection, Document
-
+Object.getOwnPropertyNames(NodeList.prototype);
+Object.getOwnPropertyNames(HTMLCollection.prototype);
+Object.getOwnPropertyNames(Document.prototype);
 
 // Liste os construtores nos dados abaixo;
 const li = document.querySelector('li');
 
 li;
+console.log(li.constructor.name); // HTMLLIElement
 li.click;
+console.log(li.click.constructor.name); // Function
 li.innerText;
+console.log(li.innerText.constructor.name); // String
 li.value;
+console.log(li.value.constructor.name); // Number
 li.hidden;
+console.log(li.hidden.constructor.name); // Boolean
 li.offsetLeft;
+console.log(li.offsetLeft.constructor.name); // Number
 li.click();
+// console.log(li.click().constructor.name);  undefined
 
 // Qual o construtor do dado abaixo:
 li.hidden.constructor.name;
+console.log(li.hidden.constructor.name.constructor.name); // String
