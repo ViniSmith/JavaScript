78fcacaeccc177a3d4d375f915a70d48db86e466
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-02-09 10:49:56 -0300

    Salvando alguns exercícios já feitos

--- animais-fantasticos-projeto-curso/scriptExerc8.js
+++ animais-fantasticos-projeto-curso/scriptExerc8.js
@@ -2,10 +2,20 @@
 // Deve conter nome, sobrenome e idade
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
+function Pessoas(nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+}
+Pessoas.prototype.nomeCompleto = function() {
+    return this.nome + ' ' + this.sobrenome;
+}
+
 
 // Liste os métodos acessados por
 // dados criados em NodeList,
 // HTMLCollection, Document
+
 
 // Liste os construtores nos dados abaixo;
 const li = document.querySelector('li');
